0277dcf3fc77d94e736436bf99a32a3a48745744
kauan <kauancavalcante8p@gmail.com>
2026-03-16 10:45:10 -0300

    first commit

---
+++ src/main/java/com/example/demo/DDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDockerApplication.class, args);
+	}
+
+}
